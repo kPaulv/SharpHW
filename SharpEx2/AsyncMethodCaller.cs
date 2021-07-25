81f894d447af4ea64beeea59db76f999578a86af
Paul <pavel.k.313@mail.ru>
2021-07-25 16:34:52 +0300

    Added CPU bound operation via Task

---
+++ SharpEx2/AsyncMethodCaller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpEx2
+{
+    public class AsyncMethodCaller
+    {
+        public static async void CallRowSumAsync()
+        {
+            TaylorSum tSum = new TaylorSum();
+            var taskRes = Task.Run(() => tSum.RowSum(1.5, 0.001));
+            await taskRes;
+        }
+    }
+}
---
+++ SharpEx2/TaylorSum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SharpEx2
+{
+    public class TaylorSum
+    {
+        private long Fact(uint x)
+        {
+            long factorial = 1;
+            for (int i = 2; i <= x; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        private bool IsEven(uint k)
+        {
+            return k % 2 == 0;
+        }
+
+        public double RowSum(double x, double eps)
+        {
+            double rowSum = 0;
+            double prevSum = 0;
+            uint k = 0;
+            do
+            {
+                prevSum = rowSum;
+                if (IsEven(k))
+                {
+                    rowSum += Math.Pow(x, 2 * k + 1) / Fact(2 * k + 1);
+                }
+                else
+                {
+                    rowSum -= Math.Pow(x, 2 * k + 1) / Fact(2 * k + 1);
+                }
+                k++;
+            } while (Math.Abs(rowSum - prevSum) > eps);
+            return rowSum;
+        }
+    }
+}
